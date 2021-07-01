dbb5d37c87672be822687523c0fc9f88787da825
Cyril Colinet <cyrilcolinet.pro@gmail.com>
2021-07-01 15:52:58 +0200

    fix(index): Remove cannonical

--- index.php
+++ index.php
@@ -10,8 +10,6 @@
     <meta name="generator" content="Hugo 0.84.0">
     <title>CODAC SAMPLE PHP</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sticky-footer-navbar/">
-
     <!-- Bootstrap core CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <meta name="theme-color" content="#7952b3">
